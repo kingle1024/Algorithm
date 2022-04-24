878d0fb09b91fc4601b632200e36fdf7c8c8a13f
kingle1024 <teran1024@naver.com>
2022-04-24 22:43:15 +0900

    예산 그리디 알고리즘을 활용하여 풀이 진행

---
+++ dataStructure/src/Programmers/PGM_12982.java
@@ -0,0 +1,22 @@
+package Programmers;
+
+import java.util.*;
+// 예산_https://programmers.co.kr/learn/courses/30/lessons/12982
+public class PGM_12982 {
+    public static void main(String[] args) {
+        solution([1,3,2,5,4],9);
+    }
+    public static int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+        int result = 0;
+        for(int i=0; i<d.length; i++){
+            if(budget >= (result + d[i])){
+                result += d[i];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
