bb092d8c2c8edd86aee388f38d0c808e824a5f52
kingle1024 <teran1024@naver.com>
2022-04-24 23:06:35 +0900

    x만큼 간격이 있는 n개의 숫자 - 21억 이후 숫자부터 음수로 표시되는 현상이 있었음. - int에서 long으로 수정하여 이슈 해결

---
+++ dataStructure/src/Programmers/PGM_12954.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+import java.util.*;
+// x만큼 간격이 있는 n개의 숫자_https://programmers.co.kr/learn/courses/30/lessons/12954
+public class PGM_12982 {
+    public static void main(String[] args) {
+        long[] answer = solution(10000000,1000);
+        for(int i=0; i< answer.length; i++){
+            System.out.println(answer[i]);
+        }
+    }
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        for(int i=0; i<n; i++){
+            answer[i] = x*(new Long(i)+1);
+        }
+
+        return answer;
+    }
+}
