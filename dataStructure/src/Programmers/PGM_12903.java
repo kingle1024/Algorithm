71c5ba03fec705e0613b04577a3b2910d6e6c06e
kingle1024 <teran1024@naver.com>
2022-05-05 16:23:03 +0900

    프로그래머스 - 가운데 글자 가져오기 쉽게 풀었다.

---
+++ dataStructure/src/Programmers/PGM_12903.java
@@ -0,0 +1,17 @@
+class PGM_12903{
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public String solution(String s) {
+            String answer = "";
+            int mid = s.length()/2;
+            if(s.length() %2 ==0){ // 짝수일 때
+                answer = s.charAt(mid-1)+""+s.charAt(mid);
+            }else{
+                answer = s.charAt(mid)+"";
+            }
+            return answer;
+        }
+    }
+}
