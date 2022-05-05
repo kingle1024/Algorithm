1043d4fe2dc7bc3b8c97835d569b784f94429dd2
kingle1024 <teran1024@naver.com>
2022-05-05 16:06:35 +0900

    프로그래머스 - 최대공약수와 최소공배수 최대공약수와 최소공배수 구하는 게 처음에는 그려지지 않았는데 곰곰이 생각해서 그려나가보니 풀 수 있었다.

---
+++ dataStructure/src/Programmers/PGM_12940.java
@@ -0,0 +1,44 @@
+class PGM_12940{
+    public static void main(String[] args) {
+
+    }
+    public int[] solution(int n, int m) {
+        int[] answer = {};
+        answer = new int[2];
+        int max = 0;
+        int min = 0;
+        int commonFactor = 1; // 최대공약수
+        int commonMultiple = 0; // 최소공배수
+
+        // 최대공약수 구하는 로직
+        if(n > m){
+            min = m;
+            max = n;
+        }else{
+            min = n;
+            max = m;
+        }
+
+        for(int i=2; i<=min; i++){
+            if(n%i == 0 && m%i ==0){
+                commonFactor = i;
+            }
+        }
+        answer[0] = commonFactor;
+
+        // 최소공배수 구하는 로직
+        int i = 1;
+        while(true){
+            if((max * i) % min == 0){
+                answer[1] = max * i;
+                break;
+            }
+            i++;
+        }
+
+        return answer;
+    }
+    // 2 12 42
+    // 3  6 21
+    //    2  7
+}
