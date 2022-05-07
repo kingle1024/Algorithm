40e74be64a83ce2b9fb724115b091767b4f11048
kingle1024 <teran1024@naver.com>
2022-05-07 18:17:42 +0900

    프로그래머스(2018 카카오 블라인드 채용) - 비밀지도 - 지도를 그려주는 부분에서 answer[i]로 해야 하는데 for j문이라서 answer[j]로 해서 결과값이 이상하게 나왔었다. 자세히 회고해보니 이상해서 고쳤고, 그제서야 정상 작동했다.

---
+++ dataStructure/src/Programmers/PGM_17681.java
@@ -0,0 +1,37 @@
+class PGM_17681{
+    public static void main(String[] args) {
+
+    }
+    public static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        answer = new String[n];
+        String arr1_change = "";
+        String arr2_change = "";
+
+        for(int i=0; i<arr1.length; i++){
+            arr1_change = Integer.toBinaryString(arr1[i]);
+            arr2_change = Integer.toBinaryString(arr2[i]);
+            while(arr1_change.length() < n){
+                arr1_change = "0"+arr1_change;
+            }
+            while(arr2_change.length() < n){
+                arr2_change = "0"+arr2_change;
+            }
+//            System.out.println(arr1_change);
+//            System.out.println(arr2_change);
+
+            for(int j=0; j<n; j++){
+                if(answer[i] == null) answer[i] = "";
+                if(arr1_change.charAt(j) == '1' || arr2_change.charAt(j) == '1'){
+                    answer[i] += "#";
+//                    System.out.println("Test1:"+arr1_change.charAt(j) +" " +arr2_change.charAt(j));
+                }else{
+                    answer[i] += " ";
+//                    System.out.println("Test2:"+arr1_change.charAt(j) +" " +arr2_change.charAt(j));
+                }
+            }
+        }
+
+        return answer;
+    }
+}
