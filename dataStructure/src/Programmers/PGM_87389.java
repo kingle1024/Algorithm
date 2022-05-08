2c7d478973f5fb6477476e0efc70e5f93a2596ad
kingle1024 <teran1024@naver.com>
2022-05-08 22:17:08 +0900

    프로그래머스 - 나머지가 1이 되는 수 찾기 - 계산을 줄이려고 i=3부터 하려다가 틀렸었다. 괜히 조금 티끌 아끼려다가 오래 삽질할뻔함..

---
+++ dataStructure/src/Programmers/PGM_87389.java
@@ -0,0 +1,17 @@
+class PGM_87389{
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            for(int i=2; i<n; i++){
+                if(n % i == 1){
+                    answer = i;
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
