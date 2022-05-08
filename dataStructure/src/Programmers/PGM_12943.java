77e3fcb5317e078219bb98e35a12db2f7c64aa32
kingle1024 <teran1024@naver.com>
2022-05-08 22:09:32 +0900

    프로그래머스 - 콜라츠 추측 - 1일 때에 예외 상황을 항상 기억하고 적용하자. - 숫자가 커질 때에는 int를 넘어 long을 생각하자. (이 부분은 빠르게 캐치 가능해진 것 같다)

---
+++ dataStructure/src/Programmers/PGM_12943.java
@@ -0,0 +1,30 @@
+class PGM_12943{
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int solution(int num) {
+            if(num == 1) return 0;
+            int answer = 0;
+            long toNum;
+            int cnt = 0;
+            toNum = num;
+            while(true){
+                if(toNum % 2 == 0){
+                    toNum = toNum / 2;
+                }else{
+                    toNum = toNum * 3 + 1;
+                }
+                cnt++;
+
+                if(toNum == 1) break;
+                else if(cnt == 500) break;
+            }
+            // System.out.println(cnt);
+            if(cnt == 500) return -1;
+            answer = cnt;
+
+            return answer;
+        }
+    }
+}
