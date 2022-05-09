958e012228c44dcd7cad71db0862848236d9bd4f
kingle1024 <teran1024@naver.com>
2022-05-09 21:27:55 +0900

    프로그래머스 - 음양 더하기 - 쉽게 풀었다.

---
+++ dataStructure/src/Programmers/PGM_76501.java
@@ -0,0 +1,17 @@
+class PGM_76501{
+    public static void main(String[] args) {
+
+    }
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 123456789;
+        answer = 0;
+        for(int i=0; i<absolutes.length; i++){
+            if(signs[i]){
+                answer += absolutes[i];
+            }else{
+                answer += (absolutes[i] * -1);
+            }
+        }
+        return answer;
+    }
+}
