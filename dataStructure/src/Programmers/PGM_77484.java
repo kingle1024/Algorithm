13917e743899fe9cce45c3d7358c61bc9608650e
kingle1024 <teran1024@naver.com>
2022-05-09 21:58:23 +0900

    프로그래머스(2021 Dev-Matching 웹 백엔드 개발) - 로또으 ㅣ최고 순위와 최저 순위 - 문제 이해에 약간 시간이 걸렸지만 그 외에는 어렵지 않게 풀었다. - maxCnt와 cnt와 함께 더하면 안되었는데, 함께 더하고 있었어서 처음에 틀렸었다.

---
+++ dataStructure/src/Programmers/PGM_77484.java
@@ -0,0 +1,59 @@
+class PGM_77484{
+    public static void main(String[] args) {
+
+    }
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+        answer = new int[2];
+        int maxCnt = 0;
+        int cnt = 0;
+        for(int i=0; i<lottos.length; i++){
+            if(lottos[i] == 0){
+                maxCnt++;
+                continue;
+            }
+            for(int j=0; j<win_nums.length; j++){
+                if(lottos[i] == win_nums[j]){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(maxCnt+ " "+cnt);
+        int arr[] = new int[2];
+        arr[0] = maxCnt+cnt;
+        arr[1] = cnt;
+        if(cnt == 6) maxCnt = 6;
+        for(int i=0; i<arr.length; i++){
+            switch(arr[i]){
+                case 6:{
+                    answer[i] = 1;
+                    break;
+                }
+                case 5:{
+                    answer[i] = 2;
+                    break;
+                }
+                case 4:{
+                    answer[i] = 3;
+                    break;
+                }
+                case 3:{
+                    answer[i] = 4;
+                    break;
+                }
+                case 2:{
+                    answer[i] = 5;
+                    break;
+                }
+                default:{
+                    answer[i] = 6;
+                }
+            }
+        }
+        return answer;
+        /*
+            44, 31, 25, 1, 0, 0
+            45, 31, 19, 10, 6, 1
+        */
+    }
+}
