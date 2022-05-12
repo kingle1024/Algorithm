c5755dd6f9ba1b94272328e5f8dbe99adb30a0b4
kingle1024 <teran1024@naver.com>
2022-05-12 23:20:54 +0900

    프로그래머스 - JadenCase 문자열 만들기 - 마지막 글자에 공백이 있는 경우 처리를 해줬다.

--- dataStructure/src/Programmers/PGM_12948.java
+++ dataStructure/src/Programmers/PGM_12948.java
@@ -1,16 +1,43 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12951
+
 class PGM_12948{
     public static void main(String[] args) {
-
+//        System.out.println(solution("3people unFollowed me"));
+        System.out.println(solution("for the last week   "));
+//        System.out.println(solution("3people   unFollowed"));
     }
-    public String solution(String phone_number) {
+    public static String solution(String s) {
         String answer = "";
-        for(int i=0; i<phone_number.length(); i++){
-            if( i < phone_number.length()-4){
-                answer += "*";
+        String arr[] = s.split(" ");
+
+        for(int i=0; i<arr.length; i++){
+            if(arr.length == 1){
+                answer += lowerCase(arr[i]);
+            }else if(i == arr.length-1){
+                if(s.charAt(s.length()-1) == ' '){
+                    answer += lowerCase(arr[i])+" ";
+                }else{
+                    answer += lowerCase(arr[i])+"";
+                }
+                break;
             }else{
-                answer += (phone_number.charAt(i)+"");
+                answer += lowerCase(arr[i]) + " ";
             }
         }
+
         return answer;
+    }
+    public static String lowerCase(String s){
+        String result = "";
+        String temp = "";
+        try{
+            result = s.toLowerCase();
+            temp = result.charAt(0) +"";
+            result = temp.toUpperCase() + result.substring(1, result.length());
+
+        }catch(Exception e){
+            return s;
+        }
+        return result;
     }
 }
