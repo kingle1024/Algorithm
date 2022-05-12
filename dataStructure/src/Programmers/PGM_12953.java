21745b3cd532d3b8fa683afca83a8fcd7e97546f
kingle1024 <teran1024@naver.com>
2022-05-12 23:42:39 +0900

    프로그래머스 - N개의 최소공배수 - 최대값을 잡아서 최대값부터 1씩 늘려주면서 최소 공배수를 구하여 처리

---
+++ dataStructure/src/Programmers/PGM_12953.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class PGM_12953{
+    public static void main(String[] args) {
+
+    }
+    public int solution(int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int maxNum = arr[arr.length-1];
+        boolean check = false;
+        int cnt = 0;
+        while(true){
+            cnt = 0;
+            for(int i=0; i < arr.length; i++){
+                if(maxNum % arr[i] == 0){
+                    cnt++;
+                }else{
+                    break;
+                }
+            }
+            if(cnt == arr.length) break;
+            maxNum++;
+        }
+        answer = maxNum;
+        return answer;
+    }
+}
