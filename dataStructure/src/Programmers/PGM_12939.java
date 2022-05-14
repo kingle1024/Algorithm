b23a0da5b070820e49f3e87931e7a30458ee579d
kingle1024 <teran1024@naver.com>
2022-05-14 16:09:23 +0900

    프로그래머스 - 최댓값과 최솟값 - split으로 문자를 잘라서 int로 형 변환 해주었다. - 정렬 후에 처음과 마지막 index 숫자를 출력해주었다.

---
+++ dataStructure/src/Programmers/PGM_12939.java
@@ -0,0 +1,16 @@
+class PGM_12939{
+    public static void main(String[] args) {
+
+    }
+    public String solution(String s) {
+        String answer = "";
+        String arr[] = s.split(" ");
+        int arrInt[] = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            arrInt[i] = Integer.parseInt(arr[i]);
+        }
+        Arrays.sort(arrInt);
+        answer = arrInt[0] +" "+arrInt[arrInt.length-1];
+        return answer;
+    }
+}
