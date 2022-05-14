4c8955622178b140b31e592ccfac3a2a337bb7aa
kingle1024 <teran1024@naver.com>
2022-05-14 15:41:36 +0900

    프로그래머스 - 피보나치 수 - arr[i] = arr[i-2] + arr[i-1] 부분이 핵심이다. - 이 부분에서 계속 재사용이 사용되기 때문에 속도에서 문제되지 않는다. - 만약 재귀를 사용하면 계산량이 엄청나게 늘어나서 실패한다.

---
+++ dataStructure/src/Programmers/PGM_12945.java
@@ -0,0 +1,23 @@
+class PGM_12945{
+    public static void main(String[] args) {
+
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        answer = fibo(n)%1234567;
+        return answer;
+    }
+    public static int fibo(int n){
+        long arr[] = new long[n];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i=2; i<n; i++){
+            arr[i] = (arr[i-2] + arr[i-1]) % 1234567;
+        }
+//        for(int i=0; i<n; i++){
+//            System.out.println(i+" : "+arr[i]);
+//        }
+
+        return (int)(arr[n-2] + arr[n-1]);
+    }
+}
