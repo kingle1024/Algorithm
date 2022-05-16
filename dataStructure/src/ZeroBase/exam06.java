ed38a78df28b06f6e3dd31bed3b5028f58de757d
kingle1024 <teran1024@naver.com>
2022-05-16 23:32:02 +0900

    제로베이스 - 6번 문제 가상 대선 당선 시뮬레이션 프로그램 - 중복된 당선인 로직이 조건에 있어서 Array에 중복된 당선인 정보를 넣어서 추가해주었다. - 당선인 이름을 처음에는 switch(randNum)에서 넣어주었는데, 나중에 max 값으로 다시 찾기 번거로워져서 배열로 리팩토링 진행했다. - 출력 형식 %05.2f 찾느라 고생했다.

---
+++ dataStructure/src/ZeroBase/exam06.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class exam06 {
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        int voteArr[] = {0, 0, 0, 0};
+        String voteName[] = {"이재명", "윤석열", "심상정", "안철수"};
+        int maxNum = 10000;
+        String voteFor = "";
+        int randNum = -1;
+        for(int i=0; i<maxNum; i++){
+            randNum = r.nextInt(4);
+            switch (randNum){
+                case 0:{
+                    voteArr[0]++;
+                    break;
+                }
+                case 1:{
+                    voteArr[1]++;
+                    break;
+                }
+                case 2:{
+                    voteArr[2]++;
+                    break;
+                }
+                case 3:{
+                    voteArr[3]++;
+                    break;
+                }
+            }
+            System.out.printf("[투표진행율]: %.2f%%, %d명 투표 => %s\n", (float)(i+1)/maxNum*100, i+1, voteName[randNum]);
+            System.out.printf("[기호:1] 이재명: %05.2f%% (투표수: %d)\n", (float)voteArr[0]/maxNum*100, voteArr[0]);
+            System.out.printf("[기호:2] 윤석열: %05.2f%% (투표수: %d)\n", (float)voteArr[1]/maxNum*100, voteArr[1]);
+            System.out.printf("[기호:3] 심상정: %05.2f%% (투표수: %d)\n", (float)voteArr[2]/maxNum*100, voteArr[2]);
+            System.out.printf("[기호:4] 안철수: %05.2f%% (투표수: %d)\n", (float)voteArr[3]/maxNum*100, voteArr[3]);
+        }
+
+        int max = -1;
+        // 최대값 구하는 로직
+        for(int i=0; i<voteArr.length; i++){
+            if(max < voteArr[i]) max = voteArr[i];
+        }
+
+        // 동일한 표의 당선자가 있는지 확인하는 로직
+        int voteDuplication = 0;
+        ArrayList<Integer> array = new ArrayList<>();
+        for(int i=0; i<voteArr.length; i++){
+            if(voteDuplication > 1){
+                break;
+            }
+            if(max == voteArr[i]){
+                array.add(i);
+                voteDuplication++;
+            }
+        }
+
+        // 출력 로직
+        if(array.size() > 1){
+            System.out.print("[투표 결과] 중복된 당선인이 존재합니다.(");
+            for (int i = 0; i < array.size(); i++) {
+                if(i == array.size()-1) {
+                    System.out.print(voteName[array.get(i)]);
+                }
+                else{
+                    System.out.print(voteName[array.get(i)]+", ");
+                }
+            }
+            System.out.print(")\n");
+        }else{
+            System.out.println("[투표 결과] 당선인: "+voteName[array.get(0)]);
+        }
+
+    }
+}
