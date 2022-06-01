dc4d6d6911bc8f30486357a882ce5c52883a8451
kingle1024 <teran1024@naver.com>
2022-06-01 23:10:25 +0900

    배열 arr의 데이터 순서를 거꾸로 변경 - Arrays.sort만 사용하다가 오랜만에 직접 구현하려니 처음에 약간 헷갈렸음. - 처음과 마지막을 비교하면서 중간까지 진행

---
+++ dataStructure/src/ZeroBase/Part2_Chapter02_Class03_Practice03.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Part2_Chapter02_Class03_Practice03 {
+    // 배열 arr의 데이터 순서를 거꾸로 변경하세요.
+    // 추가 배열을 사용하지 않고 구현
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 5, 7, 9, 11};
+        int temp;
+        for (int i = 0; i < arr.length/2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
