884d499a4b4004d2de7bbace04b990c040394c59
kingle1024 <teran1024@naver.com>
2022-06-12 21:52:03 +0900

    성능 최적화 시도 - Array -> Queue로 변경 - Scanner -> BufferedReader로 변경 - 하지만 아쉽게도 성능이 거의 동일했다..

--- Programmer/src/Baekjoon/BOK_1874.java
+++ Programmer/src/Baekjoon/BOK_1874.java
@@ -1,58 +1,86 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Stack;
-
+package Baekjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+/*
+5
+3
+2
+1
+4
+5
++
++
++
+-
+-
+-
++
+-
++
+-
+ */
 public class BOK_1874 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         solution();
     }
-    static void solution(){
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int arr[] = new int[num];
-        for(int i = 0; i < num; i++){
-            arr[i] = sc.nextInt();
+    static void solution() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Queue<String> q = new LinkedList();
+        int num = Integer.parseInt(bf.readLine());
+        Queue<Integer> sequence = new LinkedList();
+
+        int maxNum = -1;
+        for(int i=0; i<num; i++){
+            sequence.add(Integer.parseInt(bf.readLine()));
         }
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack();
 
-        for(int i = num-1; i>=0; i--){
-            stack.add(arr[i]);
-        }
+        boolean check = false;
+        while(!sequence.isEmpty()){
+            int sNum = sequence.poll();
 
-        ArrayList<String> array = new ArrayList<>();
-
-        int idx = 0;
-        int preNum = 0;
-        int maxNum = 1;
-        int popNum = 0;
-        while (true) {
-            boolean check = false;
-            if(stack.empty()) break;
-            // 넣는 경우
-            popNum = stack.pop();
-            for(int i=maxNum; i<=popNum; i++){
-                array.add("+");
-                if(i == popNum){ // 해당하는 숫자를 빼줌
-                    array.add("-");
+            if(stack.empty()){
+                if(!check) {
+                    for (int i = 1; i <= sNum; i++) {
+                        stack.add(i);
+                        q.add("+");
+                    }
+                }else{
+                    for(int i=maxNum+1; i<=sNum; i++){
+                        stack.add(i);
+                        q.add("+");
+                    }
+                }
+                stack.pop();
+                q.add("-");
+                if(maxNum < sNum){
+                    maxNum = sNum;
                 }
                 check = true;
-            }
-            if(!check) stack.add(popNum);
-
-            // 다음 넣는 경우를 위해서 처리해주는 부분
-            if(maxNum < popNum) maxNum = popNum +1;
-
-            if(stack.empty()) break;
-            // 빼는 경우
-            int popNum2 = stack.pop();
-            if(popNum >= popNum2){
-                array.add("-");
             }else{
-                stack.add(popNum2);
+                if(stack.peek() == sNum){
+                    stack.pop();
+                    q.add("-");
+                }else if(stack.peek() < sNum){
+                    for(int i=maxNum+1; i<=sNum; i++){
+                        stack.add(i);
+                        q.add("+");
+                    }
+                    stack.pop();
+                    q.add("-");
+                    if(maxNum < sNum){
+                        maxNum = sNum;
+                    }
+                }else{
+                    System.out.println("NO");
+                    return;
+                }
             }
         }
-        for(int i=0; i< array.size(); i++){
-            System.out.println(array.get(i));
+        while(!q.isEmpty()){
+            System.out.println(q.poll());
         }
     }
 }
