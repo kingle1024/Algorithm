77dc344637ea52be0c77fa3caa70c74d58b2e6c0
kingle1024 <teran1024@naver.com>
2022-09-19 22:35:36 +0900

    알람 시계 - 시간+분을 모두 분으로 합쳐서 진행 - 전체 분을 뺏을 때에 0보다 작아지면, 시간에 *24를 해서 다시 빼기 진행

---
+++ Programmer/src/Baekjoon/BOK_2884.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOK_2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int hh = Integer.parseInt(st.nextToken());
+        int mm = Integer.parseInt(st.nextToken());
+
+        int totalMM = hh*60 + mm;
+        if(totalMM -45 < 0){
+            totalMM = 24*60 + mm;
+            totalMM -= 45;
+        }else {
+            totalMM -= 45;
+        }
+//        System.out.println(totalMM);
+        int reusltHH = totalMM/60;
+        int resultMM = totalMM - (reusltHH * 60);
+        System.out.println(reusltHH + " " + resultMM);
+    }
+}
