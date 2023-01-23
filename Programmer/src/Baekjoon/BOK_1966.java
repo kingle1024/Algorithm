9a8d3e4a5a51c141dba4c07dc22443b206d11f45
kingle1024 <teran1024@naver.com>
2023-01-23 18:39:31 +0900

    프린터 큐 - 안쓰는 코드 제거

--- Programmer/src/Baekjoon/BOK_1966.java
+++ Programmer/src/Baekjoon/BOK_1966.java
@@ -25,7 +25,7 @@ public class BOK_1966 {
             insertQueue(queue, st, N, M);
             int cnt = 0;
 
-            while(!queue.isEmpty()) {
+            while (!queue.isEmpty()) {
                 Print print = queue.poll();
                 boolean isOver = false;
 
@@ -37,105 +37,19 @@ public class BOK_1966 {
                     }
                 }
 
-                if(!isOver){
+                if (!isOver) {
                     cnt++;
-                    if(print.idx == M){
+                    if (print.idx == M) {
                         sb.append(cnt).append("\n");
                         break;
                     }
                 }
             }
-
-
-
-//            int idx = 0;
-//            while (!queue.isEmpty()) {
-//
-//                Print print = queue.poll();
-//                if (now < print.num) {
-//                    idx++;
-//                    break;
-//                }
-//
-//                if (print.is) break;
-//            }
-//
-//            sb.append(idx).append("\n");
         }
+
         System.out.println(sb);
 
 
-//        for (int i = 0; i < loop; i++) {
-//            Queue<Print> queue = new ArrayDeque<>();
-//            StringTokenizer st = new StringTokenizer(br.readLine());
-//            int arrLength = Integer.parseInt(st.nextToken());
-//            int procedure = Integer.parseInt(st.nextToken());
-//
-//            st = new StringTokenizer(br.readLine());
-//
-//            if(arrLength <= 1){
-//                sb.append("1").append("\n");
-//                continue;
-//            }
-//
-//            // p번째로 출력되는 값 출력 필요
-//            for (int j = 0; j < arrLength; j++) {
-//                int n = Integer.parseInt(st.nextToken());
-//                if(j == procedure){
-//                    queue.add(new Print(true, n, j));
-//                }else {
-//                    queue.add(new Print(false, n, j));
-//                }
-//                max = Math.max(n, max);
-//            }
-//
-//            Print print = queue.poll();
-//            int startNum = print.num;
-//
-//            int cnt = 0;
-//            if (max == startNum) {
-//                cnt++;
-//                if(print.is){
-//                    cnt++;
-//                    sb.append(cnt).append("\n");
-//                    break;
-//                }
-//            }else{
-//                queue.add(print);
-//            }
-//
-//
-//            while (true) {
-//                // 하나 꺼내고
-//                Print poll = queue.poll();
-//                if(poll.is){
-//                    cnt++;
-//                    sb.append(cnt).append("\n");
-//                    break;
-//                }
-//
-//                Print next = queue.poll();
-//                queue.add(next);
-//
-//                // 다 돌면서 자기보다 큰 값이 있는지 확인
-//                while(true){
-//                    Print recurPoll = queue.peek();
-//                    // 다음이 본인이면 끝. 즉 본인이 가장 높음
-//                    if(recurPoll.idx == next.idx){
-//                        cnt++;
-//                        queue.poll();
-//                        break;
-//                        // 본인보다 높은게 있으면
-//                    }else if(recurPoll.num > poll.num){
-//                        break;
-//                    }
-//                }
-//            }
-//
-//
-//
-//        }
-//        System.out.println(sb);
     }
 
     private static void insertQueue(Queue<Print> queue, StringTokenizer st, int N, int M) {
