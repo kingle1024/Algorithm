7d6320812b31c9452b4ad6fa1804630fe56f2d46
kingle1024 <kingle1024>
2023-02-23 16:21:18 +0900

    소스 추가

---
+++ Programmer/src/Baekjoon/BOK_2630.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class BOK_2630 {
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int loop = Integer.parseInt(br.readLine());
+
+    }
+}
