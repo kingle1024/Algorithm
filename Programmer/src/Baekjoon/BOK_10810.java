9a93371c3fa09a20363f64f900ac335f1ea59d13
kingle1024 <teran1024@naver.com>
2023-08-20 21:54:14 +0900

    feat: 그림 문제

---
+++ Programmer/src/Baekjoon/BOK_10810.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOK_10810 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N+1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            for (int j = start; j <= end; j++) {
+                arr[j]= num;
+            }
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print(arr[i] +" " );
+        }
+    }
+}
