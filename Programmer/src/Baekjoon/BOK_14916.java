cfb71ab05271ad3ab7e48c8c457a60d31b63889c
kingle1024 <teran1024@naver.com>
2024-04-17 23:17:58 +0900

    Create BOK_14916.java

---
+++ Programmer/src/Baekjoon/BOK_14916.java
@@ -0,0 +1,39 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class BOK_14916 {
+  public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int amount = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(minCoins(amount)));
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static int minCoins(int amount) {
+        int[] dp = new int[amount + 1];
+        int[] coins = {2, 5};
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+}
