cf2e13df449d4bd75bc6da97ed23b767c19bd090
kingle1024 <teran1024@naver.com>
2024-04-28 21:59:40 +0900

    Create BOK_1652.java

---
+++ Programmer/src/Baekjoon/BOK_1652.java
@@ -0,0 +1,61 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOK_1652 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        if(n == 1) {
+            bw.write("0 0\n");
+        }
+        char[][] arr = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        int resultRowCnt = 0;
+        int resultColCnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean isRow = false;
+            boolean isCol = false;
+            for (int j = 0; j < n-1; j++) {
+                if (arr[i][j] == '.' && arr[i][j+1] == '.') {
+                    if(!isRow) {
+                        resultRowCnt += 1;
+                        isRow = true;
+                    }
+                }
+
+                if (arr[j][i] == '.' && arr[j+1][i] == '.') {
+                    if(!isCol) {
+                        resultColCnt += 1;
+                        isCol = true;
+                    }
+                }
+            }
+        }
+        // a[0][0] a[1][0]
+        // a[0][1] a[1][1]
+        bw.write(resultRowCnt + " " + resultColCnt + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+}
