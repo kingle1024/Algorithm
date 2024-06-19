cd315872e6f42ec5c44818e7b4f1cd12fe43b3fc
kingle1024 <teran1024@naver.com>
2024-06-19 23:10:17 +0900

    Update BOK_1268.java

--- Programmer/src/Baekjoon/BOK_1268.java
+++ Programmer/src/Baekjoon/BOK_1268.java
@@ -16,19 +16,21 @@ public class BOK_1268 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        // 배열에 값 넣기
         int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][N];
+        int[][] arr = new int[N][5];
         for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < 5; j++) {
                 int num = Integer.parseInt(st.nextToken());
                 arr[i][j] = num;
             }
         }
 
-        int[][] cntArr = new int[N][N];
+        //
+        int[][] cntArr = new int[N][5];
         for (int y = 0; y < N; y++) {
-            for (int x = 0; x < N; x++) {
+            for (int x = 0; x < 5; x++) {
                 int n = arr[y][x];
                 for (int i = 0; i < N; i++) {
                     if(n == arr[i][x]) {
@@ -43,7 +45,7 @@ public class BOK_1268 {
         int maxCnt = -1;
         for (int i = 0; i < N; i++) {
             int cnt = 0;
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < 5; j++) {
                 cnt += cntArr[i][j];
             }
             rank[i] = cnt;
