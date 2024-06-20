84d5e200d5b55a051e1df0ea1efa7cc154fceae3
kingle1024 <teran1024@naver.com>
2024-06-20 23:05:27 +0900

    Update BOK_1268.java

--- Programmer/src/Baekjoon/BOK_1268.java
+++ Programmer/src/Baekjoon/BOK_1268.java
@@ -10,14 +10,20 @@ import java.util.StringTokenizer;
 public class BOK_1268 {
 
     public static void main(String[] args) throws IOException {
-        extracted();
+        process();
     }
-    private static void extracted() throws IOException {
+    private static void process() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // 배열에 값 넣기
         int N = Integer.parseInt(br.readLine());
+        List<Set<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            Set<Integer> set = new HashSet<>();
+            list.add(set);
+        }
+
         int[][] arr = new int[N][5];
         for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,35 +37,41 @@ public class BOK_1268 {
         int[][] cntArr = new int[N][5];
         for (int y = 0; y < N; y++) {
             for (int x = 0; x < 5; x++) {
-                int n = arr[y][x];
-                for (int i = 0; i < N; i++) {
-                    if(n == arr[i][x]) {
-                        cntArr[y][x]++;
+                int myNum = arr[y][x];
+
+                // 학생수 만큼 돌면서 자기 번호와 맞는 학생을 찾는다.
+                for (int yy = 0; yy < N; yy++) {
+                    if(myNum == arr[yy][x]) {
+                        list.get(y).add(yy); // 찾으면 학생의 번호를 넣는다.
                     }
                 }
             }
         }
 
-        int[] rank = new int[N];
-        int max = -1;
-        int maxCnt = -1;
-        for (int i = 0; i < N; i++) {
-            int cnt = 0;
-            for (int j = 0; j < 5; j++) {
-                cnt += cntArr[i][j];
-            }
-            rank[i] = cnt;
-
-            if(max < cnt) {
-                max = cnt;
-                maxCnt = i;
-            }
-        }
-
-        bw.write((maxCnt+1) + "");
+        int index = findIndexOfLargestSet(list);
+        bw.write((index+1) + "");
 
         bw.flush();
         bw.close();
+    }
+
+    public static int findIndexOfLargestSet(List<Set<Integer>> list) {
+        if (list == null || list.isEmpty()) {
+            throw new IllegalArgumentException("리스트는 null이거나 비어 있을 수 없습니다.");
+        }
+
+        int maxIndex = 0;
+        int maxSize = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            Set<Integer> set = list.get(i);
+            if (set.size() > maxSize) {
+                maxSize = set.size();
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
     }
 
 }
