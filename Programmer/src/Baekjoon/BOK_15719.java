fabb1d178e13408ebbfff5918e20e324d0a2ea81
kingle1024 <teran1024@naver.com>
2024-08-20 20:34:51 +0900

    Create BOK_15719.java

---
+++ Programmer/src/Baekjoon/BOK_15719.java
@@ -0,0 +1,38 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOK_15719 {
+
+    // 15719
+    public static void main(String[] args) throws IOException {
+        process();
+    }
+
+    private static void process() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10_000_001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(arr[num] == 0) {
+                arr[num] = 1;
+            } else {
+                bw.write(num + "");
+                break;
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
