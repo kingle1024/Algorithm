61922d7accfe623a7a1b3013492cfb2f777ac36c
kingle1024 <teran1024@naver.com>
2024-08-22 20:36:29 +0900

    Create BOK_2563.java

---
+++ Programmer/src/Baekjoon/BOK_2563.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class test2 {
+
+    // 2563
+    public static void main(String[] args) throws IOException {
+        process();
+    }
+
+    private static void process() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int loop = Integer.parseInt(br.readLine());
+        int[][] arr = new
+        for (int i = 0; i < loop; i++) {
+
+        }
+
+        bw.write(mosaic + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
