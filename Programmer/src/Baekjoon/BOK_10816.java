6b3f02c32e51f3b094764abee5a612d0da59898e
kingle1024 <teran1024@naver.com>
2024-09-10 20:51:18 +0900

    Create BOK_10816.java

---
+++ Programmer/src/Baekjoon/BOK_10816.java
@@ -0,0 +1,48 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOK_10816 {
+
+    // 10816
+    public static void main(String[] args) throws IOException {
+        process();
+    }
+
+    private static void process() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            bw.write(map.getOrDefault(num, 0) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    /*
+    10
+    6 3 2 10 10 10 -10 -10 7 3
+    8
+    10 9 -5 2 3 4 5 -10
+     */
+
+}
